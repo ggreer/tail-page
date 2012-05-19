dc0e591f6145097f762e918fff2fc01bb6fd49fa
Geoff Greer <geoff@greer.fm>
2012-05-18 23:50:20 -0700

    Kill this

--- background.js
+++ background.js
@@ -10,10 +10,6 @@ function check_url(tab_id, change_info, tab) {
             break;
         }
     }
-
-    if(tab.url === "https://reach-bb.k1k.me/waterfall") {
-        chrome.pageAction.show(tab_id);
-    }
 }
 
 chrome.tabs.onUpdated.addListener(check_url);
