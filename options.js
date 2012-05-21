326f14d3b8b943d30effc73c16997d93ebc1eb7d
Geoff Greer <geoff@greer.fm>
2012-05-20 21:04:59 -0700

    Flash a saved confirmation thing

--- options.js
+++ options.js
@@ -13,12 +13,15 @@ function restore() {
 function save() {
     var url_list_node = document.getElementById("url_list");
     var urls = url_list_node.value.split("\n");
+    var saved_image = document.getElementById('saved_ok');
 
     localStorage.urls = JSON.stringify(urls);
+
+    saved_ok.style.display = "block";
+    window.setTimeout(function () { saved_ok.style.display = "none"; }, 1000);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('save').addEventListener('click', save);
     restore();
 });
-
